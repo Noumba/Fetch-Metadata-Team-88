716a1df35c4b593dc3b654580e5fd1954a555452
musawakiliML <musawakiliml@hotmail.com>
2022-08-12 03:55:27 +0100

    added signals

--- Fetch_Meta_Data_App/admin.py
+++ Fetch_Meta_Data_App/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Profile, Files, Metadata
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Files)
+admin.site.register(Metadata)
--- Fetch_Meta_Data_App/signals.py
+++ Fetch_Meta_Data_App/signals.py
@@ -1,10 +1,15 @@
-
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
 
 
-def save_profile(sender, instance, **kwargs):
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
     instance.profile.save()
-
-
-post_save.connect(save_profile, sender=User)
